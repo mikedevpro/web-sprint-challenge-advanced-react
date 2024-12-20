f3607316401358dccc23aac53b6403ecc9b7f594
Michael Nobles <Mnobles33@gmail.com>
2024-12-19 22:07:30 -0700

    next commit

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const URL = 'http://localhost:9000/api/result'
 
 // Suggested initial states
-const initialMessage = ''
+const initialMessage = '(2, 2)'
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
@@ -12,63 +12,128 @@ const initialIndex = 4 // the index the "B" is at
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
-  const [message, setMessage] = useState(initialMessage);
-  const [email, setEmail] = useState(initialEmail);
-  const [steps, setSteps] = useState(initialSteps);
-  const [index, setIndex] = useState(initialIndex); // the index the "B" is at
+  const [state, setState] = useState({
+    message: initialMessage,
+    email: initialEmail,
+    steps: initialSteps,
+    index: initialIndex,
+    response:'',
+  })// the index the "B" is at
 
-  function getXY() {
-
+  const getXY = () => {
+    const { index } = state;
+    const x = (index % 3) + 1;
+    const y = Math.floor(index / 3) + 1;
+    return { x, y }
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
   }
 
-  function getXYMessage() {
+  const getXYMessage = () => {
+    const { x, y } = getXY();
+    return `Coordinates (${x}, ${y})`;
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-  }
+  };
 
   const reset = () => {
-    setMessage({...message, message: ''});
-    setEmail({...email, email: ''});
-    setSteps({...steps, steps: 0})
-    setIndex({...index, index: 4})
+    setState({
+      ...state,
+      message: '',
+      email: '',
+      index: initialIndex,
+      steps: initialSteps,
+      response: '',
+    });
+    
     // Use this helper to reset all states to their initial values.
-  }
+  };
 
-  function getNextIndex(direction) {
-    let nextIndex = index
+  const getNextIndex = (direction) => {
+    const { index } = state;
+    const currentX = index % 3;
+    const currentY = Math.floor(index / 3);
+    let newX = currentX;
+    let newY = currentY;
+
+    if (direction === 'left') {
+      nexX = Math.max(currentX - 1, 0);
+    } else if (direction === 'up') {
+      newY = Math.max(currentY - 1, 0);
+    } else if (direction === 'right') {
+      newX = Math.min(currentX + 1, 2);
+    } else if (direction === 'down') {
+      newY = Math.min(currentY + 1, 2)
+    }
+
+    const newIndex = newY * 3 + newX;
+    return newIndex;
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-  }
+  };
 
-  function move(evt) {
+  const move = (direction) => {
+    const newIndex = getNextIndex(direction);
+
+    if (newIndex !== state.index) {
+      setState(prevState => ({
+        ...prevState,
+        index: newIndex,
+        steps: prevState.steps + 1,
+        response: '',
+      }));
+    } else {
+      let response = '';
+      if (direction === 'up') {
+        response = "You can't go up";
+      } else if (direction === 'down') {
+        response = "You can't go down"
+      } else if (direction === 'left') {
+        response = "You can't go left"
+      } else if (direction === 'right') {
+        response = "You can't go right"
+      }
+      setState({ ...state, response });
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
-  }
+  };
 
   const onChange = (evt) => {
-    const{id, value} = evt.target
-    if (id === 'email'){
-      setEmail (value)
-    }
-  }
+    const { id, value } = evt.target
+    setState({ ...state, [id]: value});
+    };
+  
     // You will need this to update the value of the input.
   
 
   const onSubmit = (evt) => {
+    evt.preventDefault();
+    const { x, y } = getXY()
+    axios.post('http://localhost:9000/api/result', {...state, x, y})
+      .then((res) => {
+        setState(prevState => ({
+          ...prevState,
+          response: res.data.message,
+          email: initialEmail,
+        }));
+      })
+      .catch((err) => {
+        setState({ ...state, email: '', response: err.response.data.message });
+      });
+
     // Use a POST request to send a payload to the server.
     
-  }
+  };
 
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{`Coordinates ${getXYMessage()}`}</h3>
-        <h3 id="steps">You moved {steps} time$</h3>
+        <h3 id="coordinates">Coordinates {getXYMessage()}</h3>
+        <h3 id="steps">You moved {state.steps} {state.steps !== 1 ? 'times' : 'time'}</h3>
       </div>
       <div id="grid">
         {
@@ -80,7 +145,7 @@ export default function AppFunctional(props) {
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{state.response}</h3>
       </div>
       <div id="keypad">
         <button onClick = {() => move('left')} id="left">LEFT</button>
@@ -90,8 +155,18 @@ export default function AppFunctional(props) {
         <button onClick = {() => reset()} id="reset">reset</button>
       </div>
       <form onSubmit={onSubmit}>
-        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
-        <input id="submit" type="submit" value="submit"></input>
+        <input 
+          id="email" 
+          type="email" 
+          placeholder="type email" 
+          onChange={onChange} 
+          value={state.email}
+          />
+        <input 
+          id="submit" 
+          type="submit" 
+          value="submit"
+          />
       </form>
     </div>
   )
